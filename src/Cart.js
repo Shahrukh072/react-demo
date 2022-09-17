5d093805b36bf1c55d12ea54af8e46ce98c23c83
Shahrukh072 <shahrukh.khan072@gmail.com>
2022-09-17 23:26:10 +0530

    add some Cart List

---
+++ src/Cart.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import CartItem from './CartItem';
+
+class Cart extends React.Component {
+  render () {
+    const arr = [1,2,3,4,5];
+    return (       
+      <div className='cart'>
+        < CartItem />
+        < CartItem />
+        < CartItem />
+      </div>
+    );
+  }
+}
+
+
+export default Cart;
+
