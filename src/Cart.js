bfebc6d206089184f83bf124ebf83bf1767d6c46
Shahrukh072 <shahrukh.khan072@gmail.com>
2022-09-18 11:22:06 +0530

    add props to cart item

--- src/Cart.js
+++ src/Cart.js
@@ -2,13 +2,50 @@ import React from 'react';
 import CartItem from './CartItem';
 
 class Cart extends React.Component {
+  constructor (){
+    super();
+    this.state = {
+       products: [
+        {
+          Price: 99,
+          title: 'Watch',
+          qty: 1,
+          img: '',
+          id: 1
+        },
+        {
+          Price: 999,
+          title: 'Mobile Phone',
+          qty: 10,
+          img: '',
+          id: 2
+        },
+        {
+          Price: 999,
+          title: 'Laptop',
+          qty: 4,
+          img: '',
+          id: 3
+        }
+       ]
+    }
+   // this.increaseQuantity = this.increaseQuantity.bind(this);
+//    this.testing();
+}
   render () {
-    const arr = [1,2,3,4,5];
+    const { products } = this.state;
     return (       
       <div className='cart'>
-        < CartItem />
-        < CartItem />
-        < CartItem />
+         {products.map((product) =>{
+          return (
+             < CartItem 
+             product = {product} 
+             key={product.id} 
+            
+             />
+          )
+
+         })}
       </div>
     );
   }
